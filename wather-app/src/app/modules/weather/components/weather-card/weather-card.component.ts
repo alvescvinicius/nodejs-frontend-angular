06f80dca6f87095d584976f1acfcbe69eaddf0c5
Carlos Vinicius de Oliveira Alves <calves@brasilcap.com.br>
2023-09-20 09:13:11 -0300

    12. Entendendo conceito de Smart e Dumb components e @Input

--- wather-app/src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ wather-app/src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Weather } from 'src/app/models/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: []
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
+
+  // ! - pq possivelmente sera inciado como nulo / indefined
+  @Input() weatherDatasInput!: Weather;
+
+  ngOnInit(): void {
+    console.log('DADOS RECEBIDOS DO COMPONENTE PAI - WEATHER-CARD: ',
+      this.weatherDatasInput);
+  }
 
 }
