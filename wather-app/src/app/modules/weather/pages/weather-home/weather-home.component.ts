c51315efac449212871307fe39a4e79de676564c
Carlos Vinicius de Oliveira Alves <calves@brasilcap.com.br>
2023-09-20 07:45:18 -0300

    11. Evitando memory leak

--- wather-app/src/app/modules/weather/pages/weather-home/weather-home.component.ts
+++ wather-app/src/app/modules/weather/pages/weather-home/weather-home.component.ts
@@ -1,13 +1,18 @@
 import { Weather } from 'src/app/models/interfaces/weather.interface';
 import { WeatherService } from './../../services/weather.service';
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+
+  // Evitar memory data leak
+  private readonly destroy$: Subject<void> = new Subject();
 
   initialCityName = 'Rio De Janeiro';
   weatherDatas!: Weather;
@@ -21,12 +26,23 @@ export class WeatherHomeComponent implements OnInit {
 
   getWeatherDatas(cityName: string): void {
     this.weatherService.getWeatherDatas(cityName)
+    .pipe(
+      takeUntil(this.destroy$)
+    )
     .subscribe({
       next: (response) => {
         response && (this.weatherDatas = response);
         console.log(this.weatherDatas);
       },
       error:(error) => console.log(error),
-    })
+    });
+
   }
+
+  // OnDestroy sempre no final da pagina
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
