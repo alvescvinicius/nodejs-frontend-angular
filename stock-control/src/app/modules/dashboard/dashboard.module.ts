053c383c519956ff7532f739c6d38ed856eeaf33
Carlos Vinicius de Oliveira Alves <calves@brasilcap.com.br>
2023-09-21 15:16:47 -0300

    31. Instalando biblioteca ChartJs

--- stock-control/src/app/modules/dashboard/dashboard.module.ts
+++ stock-control/src/app/modules/dashboard/dashboard.module.ts
@@ -1,14 +1,15 @@
-import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { ToolbarModule } from 'primeng/toolbar';
+import { ChartModule } from 'primeng/chart';
 
 import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
 import { DASHBOARD_ROUTES } from './dashboard.routing';
@@ -27,6 +28,7 @@ import { CookieService } from 'ngx-cookie-service';
     ToolbarModule,
     CardModule,
     ToastModule,
+    ChartModule
   ],
   providers: [MessageService, CookieService],
 })
