9ea2a16d315f696479c616a1625b1fd5f51664ff
Carlos Vinicius de Oliveira Alves <calves@brasilcap.com.br>
2023-09-27 09:43:36 -0300

    35. Trabalhando com Subjects

--- stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ stock-control/src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { ProductsService } from './../../../../services/products/products.service';
+import { Component, OnInit } from '@angular/core';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+  public productList: Array<GetAllProductsResponse> = [];
 
+  constructor(
+    private productsService: ProductsService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProductsDatas();
+  }
+
+  getProductsDatas(): void {
+    this.productsService.getAllProducts().subscribe({
+      next: (response) => {
+        if (response) {
+          this.productList = response;
+          console.log('DADOS DOS PRODUTOS', this.productList);
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao buscar produtos!',
+          life: 2500,
+        });
+      },
+    });
+  }
 }
